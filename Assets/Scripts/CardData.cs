8c1e3168c665584cf9c81765c73331792335225f
Josiah Aviso <150400184+WaywardTaco@users.noreply.github.com>
2024-06-06 16:37:58 +0800

    CardData and is stackable

---
+++ Assets/Scripts/CardData.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardData {
+    public enum CardSuit {
+        HEARTS, SPADES, DIAMONDS, CLUBS
+    }
+
+    private int _value;
+    private CardSuit _suit;
+
+    public int Value {
+        get {return _value;}
+    }
+    public CardSuit Suit {
+        get {return _suit;}
+    }
+
+    public bool isStackable(CardData stackTargetData){
+        return 
+            this._value == stackTargetData._value + 1 &&
+            this.isSuitOppositeColor(stackTargetData.Suit);
+    }
+
+    private bool isSuitOppositeColor(CardSuit comparisonSuit){
+        return (
+            ((this.Suit == CardSuit.HEARTS || this.Suit == CardSuit.DIAMONDS ) &&
+                (comparisonSuit == CardSuit.CLUBS || comparisonSuit == CardSuit.SPADES)) ||
+            ((this.Suit == CardSuit.CLUBS || this.Suit == CardSuit.SPADES ) &&
+                (comparisonSuit == CardSuit.HEARTS || comparisonSuit == CardSuit.DIAMONDS))
+        );
+    }
+}
