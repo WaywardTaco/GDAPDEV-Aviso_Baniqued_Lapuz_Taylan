7b00d38c0d1195e0e3cea244c96c2d2f5422822c
Basil Lapuz <152535736+CruisinAlong@users.noreply.github.com>
2024-06-06 17:41:03 +0800

    DockScript

--- Assets/Scripts/DockScript.cs
+++ Assets/Scripts/DockScript.cs
@@ -1,21 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class DockScript : MonoBehaviour, ISwipeable
 {
+    [SerializeField] private CardData _cardData;
+
 
     public void OnSwipe(SwipeEventArgs args)
     {
         if (args.Direction == ESwipeDirection.RIGHT)
         {
 
-            DockManager.Instance.tryDockWith(CardData dockTargetData);
-
         }
         else
         {
             Debug.Log("Swipe direction is not right");
         }
+    }
+
+    private GameObject GetHitObject(Vector2 screenPoint)
+    {
+        PointerEventData eventDataCurrentPosition = new PointerEventData(EventSystem.current);
+        eventDataCurrentPosition.position = screenPoint;
+
+        List<RaycastResult> results = new List<RaycastResult>();
+        EventSystem.current.RaycastAll(eventDataCurrentPosition, results);
+        if (results.Count > 1)
+        {
+            return results[1].gameObject;
+        }
+        return null;
+    }
+
+    public void Dock(object sender, SwipeEventArgs args)
+    {
+        GameObject hitObject = GetHitObject(this.transform.position);
+
     }
 }
